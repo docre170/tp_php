4dbd8d277d39932852294b6bc352bd74e923664e
docre170 <kutoseemmanuel@gmail.com>
2026-04-23 09:53:28 +0100

    j1
    essai

--- facturation/includes/footer.php
+++ facturation/includes/footer.php
@@ -1,13 +1,5 @@
-</div>
 </main>
 
-<footer class="footer">
-    <div class="container">
-        <p>&copy; 2026 <?php echo APP_NAME; ?> - DREAM TEAM</p>
-    </div>
-</footer>
-
-<script src="<?php echo BASE_URL; ?>assets/js/scanner.js"></script>
 </body>
 
 </html>
---
+++ facturation/includes/header.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Supermaché</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div>
+            <h1>supermaché - facturation</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">tableau de bord</a>
+                    </li>
+                    <li>
+                        <a href="">facture</a>
+                    </li>
+                    <li>
+                        <a href="">produits</a>
+                    </li>
+                    <li>
+                        <a href="">admin</a>
+                    </li>
+                    <li>
+                        <a href="">Déconnexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
